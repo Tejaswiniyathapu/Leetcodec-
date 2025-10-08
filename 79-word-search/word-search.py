839e87bc8226839656aa22cf7d99385c41d61328
Tejaswiniyathapu <tejaswani2007@gmail.com>
2025-10-08 17:56:30 +0530

    Time: 5691 ms (75.15%) | Memory: 12.5 MB (52.89%) - LeetSync

---
+++ 79-word-search/word-search.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        m, n = len(board), len(board[0])
+        
+        def dfs(i, j, index):
+            if index == len(word):
+                return True
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[index]:
+                return False
+            
+            temp = board[i][j]
+            board[i][j] = '#'  # mark as visited
+            
+            found = (dfs(i + 1, j, index + 1) or
+                     dfs(i - 1, j, index + 1) or
+                     dfs(i, j + 1, index + 1) or
+                     dfs(i, j - 1, index + 1))
+            
+            board[i][j] = temp  # backtrack
+            return found
+        
+        for i in range(m):
+            for j in range(n):
+                if dfs(i, j, 0):
+                    return True
+        return False
