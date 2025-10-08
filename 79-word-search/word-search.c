605753513a58893acc81a724bd13daa46ec6dd22
Tejaswiniyathapu <tejaswani2007@gmail.com>
2025-10-08 17:51:18 +0530

    Time: 154 ms (69.32%) | Memory: 7.9 MB (65.91%) - LeetSync

--- 79-word-search/word-search.c
+++ 79-word-search/word-search.c
@@ -1,38 +1,29 @@
+bool dfs(char** board, int i, int j, int m, int n, char* word, int index) {
+    if (word[index] == '\0') return true;
+    if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[index])
+        return false;
+
+    char temp = board[i][j];
+    board[i][j] = '#'; // mark visited
+
+    bool found = dfs(board, i + 1, j, m, n, word, index + 1) ||
+                 dfs(board, i - 1, j, m, n, word, index + 1) ||
+                 dfs(board, i, j + 1, m, n, word, index + 1) ||
+                 dfs(board, i, j - 1, m, n, word, index + 1);
+
+    board[i][j] = temp; // backtrack
+    return found;
+}
+
 bool exist(char** board, int boardSize, int* boardColSize, char* word) {
-    
-    
- int m = boardSize;
+    int m = boardSize;
     int n = boardColSize[0];
-    int len = strlen(word);
 
-   
-    bool dfs(int i, int j, int index) {
-        if (index == len) return true;
-        if (i < 0 || i >= m || j < 0 || j >= n) return false;
-        if (board[i][j] != word[index]) return false;
-
-        char temp = board[i][j];
-        board[i][j] = '#';  
-
-        bool found = dfs(i + 1, j, index + 1) ||
-                     dfs(i - 1, j, index + 1) ||
-                     dfs(i, j + 1, index + 1) ||
-                     dfs(i, j - 1, index + 1);
-
-        board[i][j] = temp;  
-        return found;
-    }
-
-    
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            if (dfs(i, j, 0)) return true;
+            if (dfs(board, i, j, m, n, word, 0))
+                return true;
         }
     }
-
     return false;
-}   
-
-
-
-
+}
