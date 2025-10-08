65d4d54fd9e3d73eb2f7e42374d09239c868388a
Tejaswiniyathapu <tejaswani2007@gmail.com>
2025-10-08 17:53:22 +0530

    Time: 151 ms (75.28%) | Memory: 7.9 MB (65.91%) - LeetSync

--- 79-word-search/word-search.c
+++ 79-word-search/word-search.c
@@ -1,29 +1,29 @@
+#include <stdbool.h>
+#include <string.h>
+
 bool dfs(char** board, int i, int j, int m, int n, char* word, int index) {
     if (word[index] == '\0') return true;
     if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[index])
         return false;
 
-    char temp = board[i][j];
-    board[i][j] = '#';
+    char c = board[i][j];
+    board[i][j] = 0;  
 
     bool found = dfs(board, i + 1, j, m, n, word, index + 1) ||
                  dfs(board, i - 1, j, m, n, word, index + 1) ||
                  dfs(board, i, j + 1, m, n, word, index + 1) ||
                  dfs(board, i, j - 1, m, n, word, index + 1);
 
-    board[i][j] = temp;
+    board[i][j] = c;  
     return found;
 }
 
 bool exist(char** board, int boardSize, int* boardColSize, char* word) {
     int m = boardSize;
     int n = boardColSize[0];
-
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
             if (dfs(board, i, j, m, n, word, 0))
                 return true;
-        }
-    }
     return false;
 }
