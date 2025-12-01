bb1a0330cbb46b856794d797bccba3abefe90adf
Tejaswiniyathapu <tejaswani2007@gmail.com>
2025-12-01 14:10:35 +0530

    Time: 53 ms (42.84%) | Memory: 94.7 MB (7.08%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
+         unordered_set<int> seen;
+        seen.reserve(nums.size());     
+        seen.max_load_factor(0.7);      
 
         for (int num : nums) {
             if (seen.count(num)) {
-                return true;      
+                return true;
             }
             seen.insert(num);
         }
-        return false;            
-    } 
+
+        return false;
+    }
     
 };
