6e673ebb4296e9a8cf17877405e523ce7ca136f3
Tejaswiniyathapu <tejaswani2007@gmail.com>
2026-02-25 22:53:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (51.31%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        int row = matrix.size();
-        if (row == 0) return ans;
-        int col = matrix[0].size();
-
-        int count = 0;
-        int total = row * col;
-
-        int strow = 0;
-        int stcol = 0;
-        int endrow = row - 1;
-        int endcol = col - 1;
-
-        while (count < total) {
-            for (int index = stcol; index <= endcol && count < total; index++) {
-                ans.push_back(matrix[strow][index]);
-                count++;
-            }
-            strow++;
-
-            for (int index = strow; index <= endrow && count < total; index++) {
-                ans.push_back(matrix[index][endcol]);
-                count++;
-            }
-            endcol--;
-
-            for (int index = endcol; index >= stcol && count < total; index--) {
-                ans.push_back(matrix[endrow][index]);
-                count++;
-            }
-            endrow--;
-
-            for (int index = endrow; index >= strow && count < total; index--) {
-                ans.push_back(matrix[index][stcol]);
-                count++;
-            }
-            stcol++;
+        vector<int> result;
+     int left =0;int top=0;
+     int right = matrix[0].size()-1   ;
+     int bottom = matrix.size()-1;
+     while (left<= right && top <= bottom ){
+        for (int i= left ;i <= right ; i++ ){
+            result.push_back(matrix[top][i]);
         }
+        top++;
+        for (int i=top;i<=bottom;i++){
+            result. push_back(matrix[i][right]);
+        }
+        right --;
+    if (top<=bottom){
+         for (int i= right;i>=left;i--){
+            result.push_back(matrix[bottom][i]);
 
-        return ans;
+    }
+        bottom --;
+
+        }
+        if (left <= right){
+            for (int i=bottom ;i>=top;i--){
+                result.push_back(matrix[i][left]);
+            }
+            left ++;
+        }
+       
+          
+
+     }
+     return result;
     }
 };
